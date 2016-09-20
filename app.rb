3c842a65e39957a5df0e96d8dea5e3bb70157339
Russell Osborne <russell@burningpony.com>
2016-09-20 14:00:43 -0400

    Have example code parse out headers

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'csv'
+require 'pry'
 
 puts "Welcome to DC Traffic and Moving Violations Data"
 
-#parking_tickets = CSV.read("simple_data/parking_feb_2016.csv") 
-#
-#puts parking_tickets.class
+parking_tickets = CSV.read("simple_data/parking_feb_2016.csv", headers: true, header_converters: :symbol)
+
+binding.pry
+
+puts "Thank you for working with DC Data"
