3ed40a0f41b3eb21b620fcb113649ad53a5bd287
Russell Osborne <russell@burningpony.com>
2016-09-22 16:33:16 -0400

    Update questions for in class, and simplify app.rb

--- app.rb
+++ app.rb
@@ -3,8 +3,4 @@ require 'pry'
 
 puts "Welcome to DC Traffic and Moving Violations Data"
 
-parking_tickets = CSV.read("simple_data/parking_feb_2016.csv", headers: true, header_converters: :symbol)
-
-binding.pry
-
 puts "Thank you for working with DC Data"
